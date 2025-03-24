4784e28b3b5ace2149b930743a4bb893caff0939
오이반찬 <62643840+Mega-Choco@users.noreply.github.com>
2025-03-25 00:48:17 +0900

    add circle component

--- src/component/Image.ts
+++ src/component/Image.ts
@@ -1,12 +1,21 @@
 import { Component } from "../lib/component";
+import { Vector2 } from "../lib/vector2";
 
 export class Image extends Component{
     image: ImageBitmap | null = null;
+    offset: Vector2 = new Vector2();
+    width: number | null = null;
+    height: number | null = null;
+
     path: string;
-    constructor(path: string){
+
+    constructor(path: string, width: number | null = null, height: number | null = null
+    ){
         super()
         
         this.path = path;
+        this.width = width;
+        this.height = height;
     }
     async init(): Promise<void> {
         const img = await this.loadImageBitmap(this.path);
@@ -15,10 +24,13 @@ export class Image extends Component{
 
     draw(context: CanvasRenderingContext2D): void {
         if(this.image != null){   
+            const offsetX = this.image.width / 2;
+            const offsetY = this.image.height / 2;
+
             context.drawImage(
                 this.image,
-                this.gameObject.transform.position.x,
-                this.gameObject.transform.position.y,
+                this.gameObject.transform.position.x - offsetX,
+                this.gameObject.transform.position.y - offsetY,
                 this.image.width,
                 this.image.height
             )    
---
+++ src/component/circle.ts
@@ -0,0 +1,26 @@
+import { Component } from "../lib/component";
+
+export class Circle extends Component{
+
+    radius: number = 0;
+    color: string = '';
+    constructor(radius: number = 0, color: string){
+        super();
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(context: CanvasRenderingContext2D): void {
+        
+        context.fillStyle = 'red';
+        context.beginPath();
+        context.arc(
+            this.gameObject.transform.position.x,
+            this.gameObject.transform.position.y,
+            this.radius,
+            0,
+            2 * Math.PI
+        );
+        context.fill();
+    }
+}
