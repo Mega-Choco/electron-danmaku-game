90edf5ee7325e2c4936666fcdca7ab19819c58a6
오이반찬 <62643840+Mega-Choco@users.noreply.github.com>
2025-03-25 01:31:40 +0900

    remove unrenamed files

--- src/actor/Bullet.ts
+++
@@ -1,7 +0,0 @@
-import { GameObject } from "../lib/game-object";
-
-export class Bullet extends GameObject{
-    constructor(){
-        super('bullet');
-    }
-}
--- src/lib/Component.ts
+++
@@ -1,8 +0,0 @@
-import { GameObject } from "./game-object"
-
-export abstract class Component{
-    gameObject!: GameObject
-    async init?(): Promise<void>
-    update?(delta: number): void
-    draw?(context: CanvasRenderingContext2D): void
-}
